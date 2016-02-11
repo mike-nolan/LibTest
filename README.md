45986457aaff7654bab933ceea079db84e788ad4
mobamike <mike@mobanode.com>
2016-02-11 13:01:05 +0000

    Bintray upload added.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LibTest
+
+[ ![Download](https://api.bintray.com/packages/mobamike/maven/lib-test/images/download.svg) ](https://bintray.com/mobamike/maven/lib-test/_latestVersion)
---
+++ libtestsdk/src/main/java/com/mobanode/libtest/LibTestUtil.java
@@ -0,0 +1,12 @@
+package com.mobanode.libtest;
+
+/**
+ * Created by mikenolan on 11/02/16.
+ */
+public class LibTestUtil {
+
+    public static String getLibTestString() {
+        return "It's working!!";
+    }
+
+}
